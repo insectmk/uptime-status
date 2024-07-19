52843f8b19a310e5ec7dfe6c43ca7dddc1050e3e
makuning <makun_ing@126.com>
2024-07-19 09:40:36 +0800

    更新API为Monitor-Specific API（每个网站独立API）

--- public/config.js
+++ public/config.js
@@ -6,11 +6,14 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'ur2546554-75de59318e03504b8e36f8bd',
-    //'m784488775-dd1ad84b209c05f8e185c33e',
-    //'m784490063-7b5da437e7f1e0d67613714d',
-    //'m784497419-de55aa09902ccb3ab22d548a',
-    //'m784496436-71a4bf7b1e3bdf7756be131b',
+    'm796898728-84b880951ab38555d1bdffa1', // Halo个人博客 API key
+    'm796898704-62232b37a9720e349d2fa99a', // Hexo个人博客 API key
+    'm797287546-1671ce0f51388744e04b9a0a', // 在线VScode API key
+    'm797287488-d0515842ec002e71388645b1', // 开源项目管理网站-禅道 API key
+    'm797287553-9e8e66f9568794e101f6b0dd', // memos一款隐私优先的轻量级笔记服务 API key
+    'm796898733-6d12c54f9e46ab8c902a1f45', // Waline评论服务端 API key
+    'm796905159-14864e22acaeb74c564f722b', // 静态资源网站 API key
+    'm796898840-8fc99da36433dffa2151b5be', // InsectMk的在线状态 API key
   ],
 
   // 日志天数
