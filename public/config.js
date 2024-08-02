a93d6716867a930f622368ea0859ddb9888e8b73
makuning <makun_ing@126.com>
2024-08-02 17:36:13 +0800

    更新API为Monitor-Specific API

--- public/config.js
+++ public/config.js
@@ -6,10 +6,10 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'm796898728-84b880951ab38555d1bdffa1', // Halo个人博客 API key
     'm796898704-62232b37a9720e349d2fa99a', // Hexo个人博客 API key
+    'm796898728-84b880951ab38555d1bdffa1', // Halo个人博客 API key
     'm797287546-1671ce0f51388744e04b9a0a', // 在线VScode API key
-    'm797287488-d0515842ec002e71388645b1', // 开源项目管理网站-禅道 API key
+    'm797371122-43869aa8490a03a2841f3992', // ShowDoc 一个非常适合IT团队的API文档、技术文档工具 API key
     'm797287553-9e8e66f9568794e101f6b0dd', // memos一款隐私优先的轻量级笔记服务 API key
     'm796898733-6d12c54f9e46ab8c902a1f45', // Waline评论服务端 API key
     'm796905159-14864e22acaeb74c564f722b', // 静态资源网站 API key
