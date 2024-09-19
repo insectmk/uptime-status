41d689cf397f09b3a2c41da9f85fa97da4865115
makuning <makun_ing@126.com>
2024-09-19 14:17:18 +0800

    删除在线vscode网站的监控

--- public/config.js
+++ public/config.js
@@ -8,7 +8,6 @@ window.Config = {
   ApiKeys: [
     'm796898704-62232b37a9720e349d2fa99a', // Hexo个人博客 API key
     'm796898728-84b880951ab38555d1bdffa1', // Halo个人博客 API key
-    'm797287546-1671ce0f51388744e04b9a0a', // 在线VScode API key
     'm797371122-43869aa8490a03a2841f3992', // ShowDoc 一个非常适合IT团队的API文档、技术文档工具 API key
     'm797287553-9e8e66f9568794e101f6b0dd', // memos一款隐私优先的轻量级笔记服务 API key
     'm796898733-6d12c54f9e46ab8c902a1f45', // Waline评论服务端 API key
