1eff6004af48c81f92cde6a9fdc3728fa465f137
makuning <makun_ing@126.com>
2024-11-01 09:15:53 +0800

    修复缓存导致所有的状态为同一网站的问题

--- src/common/uptimerobot.js
+++ src/common/uptimerobot.js
@@ -26,17 +26,16 @@ export async function GetMonitors(apikey, days) {
   };
   // 获取数据
   let response = {}
-  let cacheKey = 'uptime-response'
   // 检查缓存中是否有有效的数据
-  const cachedData = localStorage.getItem(cacheKey)
-  const cacheTime = localStorage.getItem(`${cacheKey}:time`)
+  const cachedData = localStorage.getItem(apikey)
+  const cacheTime = localStorage.getItem(`${apikey}:time`)
   if (cachedData && cacheTime && (Date.now() - cacheTime < window.Config.ApiCatchTime)) {
     response = JSON.parse(cachedData)
   } else {
     response = await axios.post(window.Config.ApiUrl, postdata, { timeout: 10000 })
     // 将新数据存储到缓存中，并记录当前时间
-    localStorage.setItem(cacheKey, JSON.stringify(response))
-    localStorage.setItem(`${cacheKey}:time`, Date.now().toString())
+    localStorage.setItem(apikey, JSON.stringify(response))
+    localStorage.setItem(`${apikey}:time`, Date.now().toString())
   }
   if (response.data.stat !== 'ok') throw response.data.error;
   return response.data.monitors.map((monitor) => {
